8b6320674b0a1bce4561362d060f15a5f6b83797
javid shabin <javid.main.act@gmail.com>
2024-06-18 23:28:20 +0530

    Create functrion for restaurant creation

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -1,3 +1,6 @@
+const { cloudinaryInstance } = require("../config/cloudineryConfig");
+const { Restaurant } = require("../models/restaurantModel");
+
 // Get all restaurants
 const getAllRestaurants = async (req, res) => {
   try {
@@ -21,6 +24,43 @@ const getRestautantById = async (req, res) => {
       return res.status(404).json({ message: "Restaurant not found" });
     }
     res.status(200).json(restaurant);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+// Create restaurant
+const createRestaurant = async (req, res) => {
+  try {
+    // Destructure datas from req.body
+    const { name, ...rest } = req.body;
+    // check if required fileds present
+    if (!name || Object.keys(rest).length === 0) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    // check if a restaurent with same name
+    const existRestaurant = await Restaurant.findOne({ name });
+    if (existRestaurant) {
+      return res.status(409).json({ message: "Restaurant already exists" });
+    }
+
+    let uploadResult;
+
+    if (req.file) {
+      console.log("Uploading file to Cloudinary...");
+      uploadResult = await cloudinaryInstance.uploader.upload(req.file.path);
+      console.log("Upload result");
+    } else {
+      console.log("No file to upload.");
+    }
+    // Save restaurant data to database
+    const restaurant = new Restaurant({
+      name,
+      ...rest,
+      image: uploadResult.secure_url || "",
+    });
+    const savedRestaurant = await restaurant.save();
+
+    res.status(201).json(savedRestaurant);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
