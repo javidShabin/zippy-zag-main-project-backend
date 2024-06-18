e3342275291dd89daecff54d1e5a114df54018d3
javid shabin <javid.main.act@gmail.com>
2024-06-18 23:27:13 +0530

    Create functionh for get restaurant using id

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -10,3 +10,18 @@ const getAllRestaurants = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+// Get a single restaurant by ID
+const getRestautantById = async (req, res) => {
+  try {
+    const restautantId = req.params.id;
+    console.log(restautantId);
+    const restaurant = await Restaurant.findById(restautantId);
+    // Check the resstauratn available in the id
+    if (!restaurant) {
+      return res.status(404).json({ message: "Restaurant not found" });
+    }
+    res.status(200).json(restaurant);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
