9446fec1e7c533fb651b092fed52846614e4308d
javid shabin <javid.main.act@gmail.com>
2024-07-10 08:31:33 +0530

    Create all controller bodys for admin and export

---
+++ controllers/adminController.js
@@ -0,0 +1,39 @@
+// Register admin
+const registerAdmin = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+// Login admin
+const loginAdmin = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+// Logout admin
+const logoutAdmin = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+// Admin profile
+const adminProfile = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+// Forget password
+const forgotAdminPassword = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+// Edite profile
+const editeAdminProfile = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+module.exports = {
+    registerAdmin,
+    loginAdmin,
+    logoutAdmin,
+    adminProfile,
+    forgotAdminPassword,
+    editeAdminProfile,
+}
