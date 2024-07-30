054e26831566803bf49c9b53df3316d1fa89df52
javid shabin <javid.main.act@gmail.com>
2024-07-30 23:54:04 +0530

    Create menu model and set api for the menu items

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const { userRouter } = require("./userRouter");
 const { adminRouter } = require("./adminRouter");
 const { restaurantRouter } = require("./restaurantRouter");
+const { menuRouter } = require("./menuRouter");
 
 const router = express.Router();
 
 router.use("/user", userRouter);
 router.use("/admin", adminRouter);
 router.use("/restaurant", restaurantRouter);
+router.use("menu", menuRouter);
 
 module.exports = { v1Router: router };
---
+++ routes/v1/menuRouter.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const { upload } = require("../../middlewares/multer");
+const {
+  createMenuItem,
+  getMenuforRestaurant,
+  getMenusByCategory,
+  searchMenuByName,
+  filterMenusByPrice,
+  updateMenu,
+  deleteMenu,
+} = require("../../controllers/menuController");
+
+const router = express.Router();
+
+// Create menu
+router.post("/create-menu", upload.single("image"), createMenuItem);
+// Get menus by restaurant id
+router.get("/menu/:restaurantId", getMenuforRestaurant);
+// Get menu by gategory
+router.get("/menu/:restaurantId/category/:category", getMenusByCategory);
+// Search menu by name
+router.get("/menu/:restaurantId/search", searchMenuByName);
+// Filter menu bt price
+router.get("/menu/:restaurantId/filter/price", filterMenusByPrice);
+// Update menu
+router.put("/update-menu/:menuId", updateMenu);
+// delete menu
+router.delete("/remove-menu/:meuId", deleteMenu);
+
+module.exports = { menuRouter: router };
