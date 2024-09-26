7b5f8782bb92d9e6330eee9198d946f5bf1b218b
javid shabin <javid.main.act@gmail.com>
2024-09-26 08:24:52 +0530

    add the restaurant filtering router and import the function

--- routes/v1/restaurantRouter.js
+++ routes/v1/restaurantRouter.js
@@ -6,12 +6,14 @@ const {
   updateRestautant,
   deleteRestaurant,
   getRestautantById,
+  getRestaurantByMenu,
 } = require("../../controllers/restaurantController");
 
 const router = express.Router();
 
 router.get("/all-restaurants", getAllRestaurants);
 router.get("/rest-details/:id", getRestautantById);
+router.get("/rest-details/menuItem/:name", getRestaurantByMenu)
 router.post("/create-restaurant", upload.single("image"), createRestaurant);
 router.put("/update-restaurant/:id", updateRestautant);
 router.delete("/delete-restaurant/:id", deleteRestaurant);
