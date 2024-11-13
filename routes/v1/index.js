b0d4dd6cf39324f9b2dd60c9e7545bd7ef668231
javid shabin <javid.main.act@gmail.com>
2024-11-13 08:43:31 +0530

    set router export and import the seller router

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -3,11 +3,13 @@ const { userRouter } = require("./userRouter");
 const { adminRouter } = require("./adminRouter");
 const { restaurantRouter } = require("./restaurantRouter");
 const { menuRouter } = require("./menuRouter");
+const { sellerRouter } = require("./selleRouter");
 
 const router = express.Router();
 
 router.use("/user", userRouter);
 router.use("/admin", adminRouter);
+router.use("/seller", sellerRouter)
 router.use("/restaurant", restaurantRouter);
 router.use("menu", menuRouter);
 
