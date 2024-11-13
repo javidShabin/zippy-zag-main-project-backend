14b4b848b49f7802a2fc17e7913e65661faf6d62
javid shabin <javid.main.act@gmail.com>
2024-11-13 08:47:33 +0530

    Protect the user router using user authentication

--- routes/v1/userRouter.js
+++ routes/v1/userRouter.js
@@ -13,14 +13,15 @@ const {
 } = require("../../controllers/userController");
 const { userAuth } = require("../../middlewares/userAuth");
 const { upload } = require("../../middlewares/multer");
+const { adminAuth } = require("../../middlewares/adminAuth");
 const router = express.Router();
 
 router.post("/register", userRegistration);
 router.post("/otpVerify", verifyOtpAndCreateUser);
 router.post("/login", userLogin);
-router.put("/forget-password", forgotPassword);
-router.get("/users-list", getAllUsers);
-router.post("/logout", userLogOut);
+router.put("/forget-password", userAuth, forgotPassword);
+router.get("/users-list", adminAuth, getAllUsers);
+router.post("/logout", userAuth, userLogOut);
 router.get("/user-profile", userAuth, userProfile);
 router.put(
   "/update-profile",
