309b5377180c7df337c9f4fc8bf6e44635f5aa36
javid shabin <javid.main.act@gmail.com>
2024-11-20 08:56:09 +0530

    Clear the errors from addess controller and set the autgentication fot address

--- controllers/addressController.js
+++ controllers/addressController.js
@@ -4,8 +4,7 @@ const { Address } = require("../models/addressModel");
 const createAddress = async (req, res) => {
   try {
     const { name, email, street, city, postalCode, country, phone } = req.body;
-    console.log(name, email);
-    const userId = req.user.id;
+    const user = req.user.id;
     if (!name || !street || !city || !postalCode || !phone) {
       return res.status(400).json({
         message: "All fields are required",
@@ -20,7 +19,7 @@ const createAddress = async (req, res) => {
       postalCode,
       phone,
       country,
-      user: userId,
+      user: user,
     });
     await address.save();
     res.status(201).json(address);
@@ -45,7 +44,6 @@ const updateAddress = async (req, res) => {
 const getAddress = async (req, res) => {
   try {
     const userId = req.user.id;
-    console.log(userId);
     const addresses = await Address.find({ user: userId });
     res.status(200).json(addresses);
   } catch (error) {}
--- routes/v1/addressRouter.js
+++ routes/v1/addressRouter.js
@@ -5,15 +5,16 @@ const {
   getAddress,
   deleteAddress,
 } = require("../../controllers/addressController");
+const { userAuth } = require("../../middlewares/userAuth");
 const router = express.Router();
 
 // Create address
-router.post("/address", createAddress);
+router.post("/create-address", userAuth, createAddress);
 // Ppdate address
-router.put("/address/:id", updateAddress);
+router.put("/update-address/:id", userAuth, updateAddress);
 // Get address
-router.get("/address", getAddress);
+router.get("/get-address", userAuth, getAddress);
 // Delete address
-router.delete("/address/:id", deleteAddress);
+router.delete("/remove-address/:id", userAuth, deleteAddress);
 
 module.exports = { addressRouter: router };
