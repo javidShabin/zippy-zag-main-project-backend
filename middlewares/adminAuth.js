2da7e43b9be2e4547c1dc9468e556a517fea4949
javid shabin <javid.main.act@gmail.com>
2024-12-10 19:31:13 +0530

    add log

--- middlewares/adminAuth.js
+++ middlewares/adminAuth.js
@@ -14,6 +14,7 @@ const adminAuth = (req, res, next) => {
     }
     // Token verification
     const verifyToken = jwt.verify(adminToken, process.env.JWT_SECRET_KEY);
+    console.log(verifyToken)
     if (!verifyToken) {
       return res.status(401).json({
         success: false,
