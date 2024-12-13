53e185911397d6269628853c52a413218cefc3b5
javid shabin <javid.main.act@gmail.com>
2024-12-14 01:51:59 +0530

    Create the order model for fetching the order details

---
+++ models/orderModel.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User", // Assuming you have a User model
+      required: true,
+    },
+    products: [
+      {
+        ItemName: { type: String, required: true },
+        price: { type: Number, required: true },
+        quantity: { type: Number, required: true },
+        image: { type: String, required: true },
+      },
+    ],
+    totalAmount: { type: Number, required: true },
+    paymentStatus: {
+      type: String,
+      enum: ["Pending", "Completed", "Failed"],
+      default: "Pending",
+    },
+    paymentSessionId: { type: String, required: true },
+    paymentMethod: { type: String, enum: ["Stripe"], required: true },
+    createdAt: { type: Date, default: Date.now },
+    address: {
+      name: { type: String, required: true },
+      email: { type: String, required: true },
+      phone: { type: String, required: true },
+      street: { type: String, required: true },
+      city: { type: String, required: true },
+      postalCode: { type: String, required: true },
+      country: { type: String, required: true },
+    },
+  },
+  { timestamps: true }
+);
+
+// Create the Order model based on the schema
+const Order = mongoose.model("Order", orderSchema);
+
+module.exports = { Order };
