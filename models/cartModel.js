e9da424ab7699e7358daf983cf844aab9ce4f008
javid shabin <javid.main.act@gmail.com>
2024-12-15 20:18:30 +0530

    add restaurant id in cart schema

--- models/cartModel.js
+++ models/cartModel.js
@@ -2,6 +2,11 @@ const mongoose = require("mongoose");
 
 const cartSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  restaurantId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Restaurant", // Assuming you have a Restaurant model
+    required: true,
+  },
   items: [
     {
       menuItem: {
