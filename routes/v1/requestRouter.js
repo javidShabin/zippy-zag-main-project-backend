0fbf7ff5f3005dd28c70002f3cf263150f167c87
javid shabin <javid.main.act@gmail.com>
2024-12-16 15:55:51 +0530

    create the router for the request controller

--- routes/v1/requestRouter.js
+++ routes/v1/requestRouter.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const { userAuth } = require("../../middlewares/userAuth");
-const { createRequest } = require("../../controllers/requestController");
+const {
+  createRequest,
+  getAllRequests,
+  getRequestById,
+  getRequestsByUserId,
+  updateRequestStatus,
+  deleteRequest,
+} = require("../../controllers/requestController");
 
 const router = express();
 
 router.post("/create-request", userAuth, createRequest);
+router.get("/getAllRequest", getAllRequests);
+router.get("/getRequestById/:requestId", getRequestById);
+router.get("/getRequestByUserId", getRequestsByUserId);
+router.put("/update/status", updateRequestStatus);
+router.delete("/delete/:requestId", deleteRequest);
 
 module.exports = { requestRouter: router };
